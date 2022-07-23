7c589be31ed0252a7bb2b1b67c1ca0763daf198b
Christian Cantrell <christian.cantrell@gmail.com>
2022-07-23 17:39:20 -0400

    Added error checking and now allows you to pass in an image path

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const tesseract = require('node-tesseract-ocr');
+const fs = require('fs');
 
 const CONFIG = {
     lang: 'eng',
     oem: 1,
     psm: 3
 };
+const OUTPUT_FILE = 'output.txt';
 
 function run() {
-    tesseract
-    .recognize("images/11.png", CONFIG)
-    .then((rawText) => {
-        let text = rawText.replace(/\r\n/g, ' ');
-      console.log(text);
+    if (process.argv.length !== 3) {
+        console.error('Pass in the path to one image.');
+        process.exit();
+    }
+    const imgPath = process.argv[2];
+    console.info(`About to OCR ${imgPath}`);
+    if (!fs.existsSync(imgPath)) {
+        console.error(`${imgPath} is not a valid path.`);
+        process.exit();
+    }
+
+    tesseract.recognize(imgPath, CONFIG)
+    .then(text => {
+        text = text.trim();
+        if (text.length === 0) {
+            console.error('Something went wrong becuase no text as found.');
+            process.exit();
+        }
+        // If you want to massage your data, now's your chance
+        // let text = rawText.replace(/\r\n/g, ' ');
+        fs.writeFileSync(`./${OUTPUT_FILE}`, text);
+        console.info(`Text written to ${OUTPUT_FILE}`);
     })
     .catch((error) => {
-      console.log(error.message)
+        console.log(error.message)
     });
 }
 
